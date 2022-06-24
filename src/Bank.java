bd00b8235849656cdc00f332cb0e13ae91ccb5e9
Anatoly <none>
2022-06-24 22:47:54 +0300

    Bank started

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Bank {
+
+	/**
+	 * the name of this bank
+	 */
+	private String name;
+	/**
+	 * the list of users of this bank
+	 */
+	private ArrayList<User> users;
+	/**
+	 * the list of accounts of this bank
+	 */
+	private ArrayList<Account> accounts;
+
 }
