b2dbdd110edb70dfa87183080cc3ccf1d44e1249
Anatoly <none>
2022-06-24 22:37:54 +0300

    Account and Transaction are started

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Account {
+
+	/**
+	 * account name
+	 */
+	private String name;
+	/**
+	 * balance on the account
+	 */
+	private double balance;
+	/**
+	 * The ID for the account
+	 */
+	private String uuid;
+	/**
+	 * the user - holder of the account
+	 */
+	private User holder;
+	/**
+	 * the list of the transactions of this account
+	 */
+	private ArrayList<Transaction> transactions;
+
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,22 @@
+import java.util.Date;
+
 public class Transaction {
+
+	/**
+	 * the amount of this transaction
+	 */
+	private double amount;
+	/**
+	 * the time and nate of this transaction
+	 */
+	private Date timestamp;
+	/**
+	 * a memo of this transaction
+	 */
+	private String memo;
+	/**
+	 * the account in wich the transaction was performed
+	 */
+	private Account inAccount;
+
 }
