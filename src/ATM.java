634e3d89bd1dd43397b892b3e93bfdd723e7c7e7
Anatoly <none>
2022-06-24 22:21:50 +0300

    user started

--- src/ATM.java
+++ src/ATM.java
@@ -1,2 +1,5 @@
 public class ATM {
+	public static void main(String[] args) {
+		;
+	}
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class User {
+
+	/**
+	 * The first name of the user
+	 */
+	private String firstName;
+	/**
+	 * The last name of the user
+	 */
+	private String lastName;
+	/**
+	 * The ID of the user
+	 */
+	private String uuid;
+	/**
+	 * The MD5 hash of the user's pin number
+	 */
+	private byte pinHash[];
+	/**
+	 * The list of user's accounts
+	 */
+	private ArrayList<Account> accounts;
+
 }
